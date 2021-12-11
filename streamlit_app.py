9a99f05f924f1d616c48696d0200951780fc67e8
jthhk <31700188+jthhk@users.noreply.github.com>
2021-12-11 14:43:36 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,19 +19,18 @@ def my_widget(key):
     with open(profile_summary_file) as f:
         profile_summary = json.load(f, object_hook=lambda d: SimpleNamespace(**d))
     
-    win_ratio =  profile_summary.tradeWins / (profile_summary.tradeWins + profile_summary.tradeLosses)
+    win_ratio =  round((profile_summary.tradeWins / (profile_summary.tradeWins + profile_summary.tradeLosses)) * 100,2)
     
     col1, col2 = st.columns([1,3])
     
     if(key == "Snail"):
-        col1.metric("Loss", str(profile_summary.historicProfitIncFees_Total), str(profile_summary.historicProfitIncFees_Percent) + "%")
-        col2.error("Loss%: " + str(profile_summary.historicProfitIncFees_Percent) + " | Loss$: " + str(profile_summary.historicProfitIncFees_Total) + " | Win: " + str(profile_summary.tradeWins) + " | Loss: " + str(profile_summary.tradeLosses) + " | WL%: " + str(win_ratio) + "%")
-        col2.write("12 Dec @ 15:30")
-    elif(key == "Scalper"):
-        col1.metric("Win", str(profile_summary.historicProfitIncFees_Total), str(profile_summary.historicProfitIncFees_Percent) + "%")
-        col2.error("Win%: " + str(profile_summary.historicProfitIncFees_Percent) + " | Win$: " + str(profile_summary.historicProfitIncFees_Total) + " | Win: " + str(profile_summary.tradeWins) + " | Loss: " + str(profile_summary.tradeLosses) + " | WL%: " + str(win_ratio) + "%")
+        col1.metric("Loss", str(round(profile_summary.historicProfitIncFees_Total),2), str(round(profile_summary.historicProfitIncFees_Percent),1) + "%")
+        col2.error("Win: " + str(profile_summary.tradeWins) + " | Loss: " + str(profile_summary.tradeLosses) + " | WL%: " + str(win_ratio) + "%")
         col2.write("12 Dec @ 15:30  |  Staretd:" + str(profile_summary.botstart_datetime))
-        
+    elif(key == "Scalper"):
+        col1.metric("Win", str(round(profile_summary.historicProfitIncFees_Total),2), str(round(profile_summary.historicProfitIncFees_Percent),1) + "%")
+        col2.error("Win: " + str(profile_summary.tradeWins) + " | Loss: " + str(profile_summary.tradeLosses) + " | WL%: " + str(win_ratio) + "%")
+        col2.write("12 Dec @ 15:30  |  Staretd:" + str(profile_summary.botstart_datetime))
     else:
         col1.metric("N/A", "0", "0%")
         col2.info("Not started")
