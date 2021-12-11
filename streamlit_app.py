317ed31c3c747589668ed0d9d89f2ab28fae8f29
jthhk <31700188+jthhk@users.noreply.github.com>
2021-12-11 12:15:18 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,18 +5,11 @@ import pandas as pd
 import streamlit as st
 
 def my_widget(key):
-    st.subheader('Hello there!')
-    clicked = st.button("Click me " + key)
-
-# This works in the main area
-clicked = my_widget("first")
+    st.subheader('JimBot updates')
 
 # And within an expander
-my_expander = st.beta_expander("Expand", expanded=True)
+my_expander = st.beta_expander("Bot1", expanded=True)
 with my_expander:
     clicked = my_widget("second")
 
-# AND in st.sidebar!
-with st.sidebar:
-    clicked = my_widget("third")
 
