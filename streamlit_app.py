8a9c51a32a0afa67050514744c3e348db8b6ffd5
jthhk <31700188+jthhk@users.noreply.github.com>
2021-12-11 12:23:33 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ import streamlit as st
 
 def my_widget(key):
     st.subheader('JimBot updates for' + key)
-    st.markdown(f"<h4 style='text-align: left; margin-left: 30px;'> Started: {started.split('.')[0]} | Running for: {run_for}</h4>", unsafe_allow_html=True)
+    st.markdown(f"<h4 style='text-align: left; margin-left: 30px;'> Started: TIME | Running for: TIME </h4>", unsafe_allow_html=True)
 
 # And within an expander
 my_expander = st.expander("Scalper", expanded=True)
